07d5bfd0e36b0bc4332540b22b6e153790f36eb8
Agnieszka Burczyk <agnieszka.fuerst@gmail.com>
2019-12-29 13:36:44 +0100

    Added tests for Slider, Droppable and some comments

--- test.js
+++ test.js
@@ -8,52 +8,54 @@ describe('Testing demoqa.com', function() {
         driver = await new Builder().forBrowser('chrome').build();
     });
 
-    // describe('General tests on demoqa.com', function() {
+    let actions;
+    before(async function() {
+        actions = await driver.actions();
+    });
 
-    //     it('Check title of demoqa.com', async function() {
-    //         await driver.get('https://demoqa.com/');
+    describe('General tests on demoqa.com', function() {
 
-    //         // // Enter keywords and click enter
-    //         // await driver.findElement(By.id('lst-ib')).sendKeys('dalenguyen', Key.RETURN);
-    //         // // Wait for the results box by id
-    //         // await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
+        it('Check title of demoqa.com', async function() {
+            await driver.get('https://demoqa.com/');
+
+            // // Enter keywords and click enter
+            // await driver.findElement(By.id('lst-ib')).sendKeys('dalenguyen', Key.RETURN);
+            // // Wait for the results box by id
+            // await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
             
-    //         let title = await driver.getTitle();
-    //         assert.equal(title, 'ToolsQA – Demo Website to Practice Automation – Demo Website to Practice Automation');
-    //     });
-    // });
+            let title = await driver.getTitle();
+            await assert.equal(title, 'ToolsQA – Demo Website to Practice Automation – Demo Website to Practice Automation');
+        });
+    });
 
 
-    // describe('Test the HTML contact form', function() {
-    //     it('After clicking Submit button you should be redirected to a search website and the url address should contain value of Country and Subject as parameters', async function() {
-    //         await driver.get('https://demoqa.com/html-contact-form/');
-    //         await driver.findElement(By.className('firstname')).sendKeys('Ala');
-    //         await driver.findElement(By.id('lname')).sendKeys('Makota');
-    //         await driver.findElement(By.name('country')).sendKeys('Poland');
-    //         await driver.findElement(By.id('subject')).sendKeys('Hello');
-    //         await driver.findElement(By.xpath(".//div[@id='content']/div[@class='demo-frame']/div[@class='container']/form/input[@type='submit']")).click();
-    //         let currentUrl = await driver.getCurrentUrl();
-    //         assert.equal(currentUrl, 'https://demoqa.com/html-contact-form/onsubmitform?country=Poland&subject=Hello');
-    //     });
-
-    //     it('After clicking Submit button you should be redirected to a search website and the url address should contain value of Country and Subject as parameters despite special characters', async function() {
-    //         await driver.get('https://demoqa.com/html-contact-form/');
-    //         await driver.findElement(By.className('firstname')).sendKeys('Ala');
-    //         await driver.findElement(By.id('lname')).sendKeys('Makota');
-    //         await driver.findElement(By.name('country')).sendKeys('P0l4nd');
-    //         await driver.findElement(By.id('subject')).sendKeys('%&*');
-    //         await driver.findElement(By.xpath(".//div[@id='content']/div[@class='demo-frame']/div[@class='container']/form/input[@type='submit']")).click();
-    //         let currentUrl = await driver.getCurrentUrl();
-    //         assert.equal(currentUrl, 'https://demoqa.com/html-contact-form/onsubmitform?country=P0l4nd&subject=%25%26*');
-    //     });
-    // });
-
-    describe('Test the Tooltip and Double click', function() {
-        let actions;
-        before(async function() {
-            actions = await driver.actions();
+    describe('Test the HTML contact form', function() {
+        it('After clicking Submit button you should be redirected to a search website and the url address should contain value of Country and Subject as parameters', async function() {
+            await driver.get('https://demoqa.com/html-contact-form/');
+            await driver.findElement(By.className('firstname')).sendKeys('Ala');
+            await driver.findElement(By.id('lname')).sendKeys('Makota');
+            await driver.findElement(By.name('country')).sendKeys('Poland');
+            await driver.findElement(By.id('subject')).sendKeys('Hello');
+            await driver.findElement(By.xpath(".//div[@id='content']/div[@class='demo-frame']/div[@class='container']/form/input[@type='submit']")).click();
+            let currentUrl = await driver.getCurrentUrl();
+            assert.equal(currentUrl, 'https://demoqa.com/html-contact-form/onsubmitform?country=Poland&subject=Hello');
+            //add improvement: assert which takes not the whole url, but only country & subject
         });
 
+        it('After clicking Submit button you should be redirected to a search website and the url address should contain value of Country and Subject as parameters despite special characters', async function() {
+            await driver.get('https://demoqa.com/html-contact-form/');
+            await driver.findElement(By.className('firstname')).sendKeys('Ala');
+            await driver.findElement(By.id('lname')).sendKeys('Makota');
+            await driver.findElement(By.name('country')).sendKeys('P0l4nd');
+            await driver.findElement(By.id('subject')).sendKeys('%&*');
+            await driver.findElement(By.xpath(".//div[@id='content']/div[@class='demo-frame']/div[@class='container']/form/input[@type='submit']")).click();
+            let currentUrl = await driver.getCurrentUrl();
+            assert.equal(currentUrl, 'https://demoqa.com/html-contact-form/onsubmitform?country=P0l4nd&subject=%25%26*');
+            //add improvement: assert which takes not the whole url, but only country & subject
+        });
+    });
+
+    describe('Test the Tooltip and Double click', function() {
         it('After double click on double click button you should see an alert window', async function() {
             await driver.get('https://demoqa.com/tooltip-and-double-click/');
             let doubleClickButton = await driver.findElement(By.id('doubleClickBtn'));
@@ -77,6 +79,63 @@ describe('Testing demoqa.com', function() {
         });
     });
 
+    describe('Test the Slider', function(){
+        it('Slider moves by dragging a mouse to the release point', async function() {
+            await driver.get('https://demoqa.com/slider/');
+            let webElementFrom = await driver.findElement(By.className('ui-slider-handle ui-corner-all ui-state-default'));
+            let webElementTo = await driver.findElement(By.className('ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content'));
+            await actions.dragAndDrop(webElementFrom, webElementTo).perform();
+            //add some different release point/webElementTo
+            //add an assert
+        });
+
+        it('Slider moves by clicking a bar to the click point', async function() {
+            await driver.get('https://demoqa.com/slider/');
+            await driver.findElement(By.className('ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content')).click();
+            //add some different click point
+            //add an assert
+        });
+    });
+
+    describe('Test the Droppable', function(){
+        it('After dragging the element to the target the element is within the target square and the target square is colored yellow with inscription `Dropped!`', async function() {
+            await driver.get('https://demoqa.com/droppable/');
+            let webElementFrom = await driver.findElement(By.id('draggable'));
+            let webElementTo = await driver.findElement(By.id('droppable'));
+            await actions.dragAndDrop(webElementFrom, webElementTo).perform();
+            // let dropped = await driver.findElement(By.xpath('.//div[@id="droppable"]/p'));
+            // assert.equal(dropped, 'Dropped!');
+        });
+
+    //     it('After dragging the element not to the target the element is placed on release point and the target square is colored gray with inscription `Drop here`', async function() {
+    //         await driver.get('https://demoqa.com/droppable/');
+    //         let webElementFrom = await driver.findElement(By.id('draggable'));
+    //     });
+
+    //     it('After dragging the element not whole to the target the element is placed on release point and the target square is colored gray with inscription `Drop here`', async function() {
+    //         await driver.get('https://demoqa.com/droppable/');
+    //         let webElementFrom = await driver.findElement(By.id('draggable'));
+    //     });
+
+    //     it('After dragging the element outside the element is nov visible and the target square is colored gray with inscription `Drop here`', async function() {
+    //         await driver.get('https://demoqa.com/droppable/');
+    //         let webElementFrom = await driver.findElement(By.id('draggable'));
+    //     });
+    });
+
+    // describe('', function(){
+    //     it('', async function() {
+
+    //     });
+
+    //     it('', async function() {
+
+    //     });
+
+    //     it('', async function() {
+
+    //     });
+    // });
 
 
 
